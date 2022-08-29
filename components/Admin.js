d5c80a93510b6c1137885fa860bbd5fdd898165d
diogoramosr <diogo.rrodrigues2003@gmail.com>
2022-08-29 10:49:12 -0300

    Revisando dashboard

--- components/Admin.js
+++ components/Admin.js
@@ -10,7 +10,7 @@ export default function Admin() {
   const [open, setOpen] = useState(true);
   const [submenuOpen, setSubmenuOpen] = useState(false);
   const Menus = [
-    { title: "Dashboard" },
+    { title: "Dashboard", submenu: true, submenuItems: [ {title: "Home"}, {} ],},
     { title: "Pages" },
     { title: "Media", spacing: true },
     {
@@ -22,15 +22,11 @@ export default function Admin() {
         { title: "Editar" },
       ],
     },
-    { title: "Historico" },
+    { title: "Histórico" },
     { title: "Inbox" },
-    { title: "Settings" },
   ];
 
-  const [show, setShow] = useState(null);
   const [profile, setProfile] = useState(false);
-  const [product, setProduct] = useState(false);
-  const [deliverables, setDeliverables] = useState(false);
 
   return (
     <div className="flex">
@@ -94,121 +90,19 @@ export default function Admin() {
         </ul>
       </div>
 
-      <div className="p-1 pt-6 w-full">
-        <nav className="xl:block hidden">
+      <div className="h-full w-full">
+        <nav className=" xl:block p-5">
           <div className="mx-auto container px-6 py-2 xl:py-0">
-            <div className="flex items-center justify-between">
-              <div className="inset-y-0 left-0 flex items-center xl:hidden">
-                <div className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-gray-100 focus:outline-none transition duration-150 ease-in-out">
-                  <div className="visible xl:hidden">
-                    <ul className="p-2 border-r bg-black absolute rounded left-0 right-0 shadow mt-8 md:mt-8 hidden">
-                      <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 flex items-center focus:text-indigo-700 focus:outline-none">
-                        <div className="flex items-center">
-                          <div className="w-12 cursor-pointer flex text-sm border-2 border-transparent rounded focus:outline-none focus:border-white transition duration-150 ease-in-out"></div>
-                          <p className="text-sm ml-2 cursor-pointer">
-                            Jane Doe
-                          </p>
-                          <div className="sm:ml-2 text-white relative">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="icon icon-tabler icon-tabler-chevron-down cursor-pointer"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            >
-                              <path stroke="none" d="M0 0h24v24H0z" />
-                              <polyline points="6 9 12 15 18 9" />
-                            </svg>
-                          </div>
-                        </div>
-                      </li>
-                      <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-                        <div className="flex items-center">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="icon icon-tabler icon-tabler-user"
-                            width={20}
-                            height={20}
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <circle cx={12} cy={7} r={4} />
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                          </svg>
-                          <span className="ml-2">Profile</span>
-                        </div>
-                      </li>
-                    </ul>
-                    <svg
-                      onclick="MenuHandler(this,true)"
-                      aria-haspopup="true"
-                      aria-label="Main Menu"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="show-m-menu icon icon-tabler icon-tabler-menu"
-                      width={28}
-                      height={28}
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" />
-                      <line x1={4} y1={8} x2={20} y2={8} />
-                      <line x1={4} y1={16} x2={20} y2={16} />
-                    </svg>
-                  </div>
-                  <div
-                    className="hidden close-m-menu text-gray-700"
-                    onclick="MenuHandler(this,false)"
-                  >
-                    <svg
-                      aria-label="Close"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" />
-                      <line x1={18} y1={6} x2={6} y2={18} />
-                      <line x1={6} y1={6} x2={18} y2={18} />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <div className="flex w-full sm:w-auto items-center sm:items-stretch justify-end sm:justify-start">
-                <div className="flex items-center">
-                  <span>Pages/Dashboard</span>
-                </div>
-              </div>
+            <div className="flex items-center justify-end">
               <div className="flex">
-                <div className="hidden xl:flex items-center">
-                  <div className="relative md:mr-6 my-2">
-                    <input className="focus:outline-none bg-gray-100 border-gray-300 border transition duration-150 ease-in-out hover:bg-gray-300 rounded text-gray-600 px-5 py-2 text-xs" />
-                  </div>
-                  <div className="ml-1 relative">
+                <div className="xl:flex items-center">
+                  <div className="ml-6 relative">
                     <div
                       className="flex items-center relative"
                       onClick={() => setProfile(!profile)}
                     >
                       {profile && (
-                        <ul className="p-2 w-40 border-r bg-white absolute rounded right-0 shadow top-0 mt-16 ">
+                        <ul className="p-2 w-40 border-r bg-white absolute rounded -right-5 shadow top-0 mt-16 ">
                           <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
                             <div className="flex items-center">
                               <svg
@@ -230,26 +124,7 @@ export default function Admin() {
                               <span className="ml-2">My Profile</span>
                             </div>
                           </li>
-                          <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none flex items-center">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="icon icon-tabler icon-tabler-help"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            >
-                              <path stroke="none" d="M0 0h24v24H0z" />
-                              <circle cx={12} cy={12} r={9} />
-                              <line x1={12} y1={17} x2={12} y2="17.01" />
-                              <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
-                            </svg>
-                            <span className="ml-2">Help Center</span>
-                          </li>
+
                           <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 flex items-center focus:text-indigo-700 focus:outline-none">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -302,115 +177,6 @@ export default function Admin() {
             </div>
           </div>
         </nav>
-        <nav>
-          <div className="flex items-center">
-            <div
-              id="menu"
-              className="text-gray-800"
-              onClick={() => setShow(!show)}
-            >
-              {show ? (
-                ""
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-menu-2"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <line x1={4} y1={6} x2={20} y2={6} />
-                  <line x1={4} y1={12} x2={20} y2={12} />
-                  <line x1={4} y1={18} x2={20} y2={18} />
-                </svg>
-              )}
-            </div>
-          </div>
-          {/*Mobile responsive sidebar*/}
-          <div
-            className={
-              show
-                ? " xl:hidden h-96 absolute z-40  transform  translate-x-0 "
-                : " xl:hidden h-96 absolute z-40  transform -translate-x-full"
-            }
-          >
-            <div
-              className="bg-red-800 opacity-50"
-              onClick={() => setShow(!show)}
-            />
-            <div className="w-64 z-40 fixed overflow-y-auto z-40 top-0 bg-white shadow h-full flex-col justify-between xl:hidden pb-4 transition duration-150 ease-in-out">
-              <div className="px-6 h-full">
-                <div className="flex flex-col justify-between h-full w-full">
-                  <div>
-                    <div className="mt-6 flex w-full items-center justify-between">
-                      <div className="flex items-center justify-between w-full">
-                        <div
-                          id="cross"
-                          className="text-gray-800"
-                          onClick={() => setShow(!show)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="icon icon-tabler icon-tabler-x"
-                            width={24}
-                            height={24}
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <line x1={18} y1={6} x2={6} y2={18} />
-                            <line x1={6} y1={6} x2={18} y2={18} />
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
-                    <ul className="f-m-m"></ul>
-                  </div>
-                  <div className="w-full pt-4">
-                    <div className="flex justify-center mb-4 w-full">
-                      <div className="relative w-full">
-                        <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="icon icon-tabler icon-tabler-search"
-                            width={16}
-                            height={16}
-                            viewBox="0 0 24 24"
-                            strokeWidth={1}
-                            stroke="#A0AEC0"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <circle cx={10} cy={10} r={7} />
-                            <line x1={21} y1={21} x2={15} y2={15} />
-                          </svg>
-                        </div>
-                        <input
-                          className="bg-gray-100 focus:outline-none rounded w-full text-sm text-gray-500  pl-10 py-2"
-                          type="text"
-                          placeholder="Search"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
-        {/* Code block ends */}
       </div>
     </div>
   );
